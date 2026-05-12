2b3de887fd3250c9ef59d5d767ecf21527ac2042
Seungkyu-Han <trust1204@gmail.com>
2026-05-12 22:56:53 +0900

    feat: improve file validators with detailed docstrings and enhanced checks

--- file_validator/file_extension_validator.py
+++ file_validator/file_extension_validator.py
@@ -1,39 +1,44 @@
 import os
-
-from fastapi import UploadFile, HTTPException, status
 from functools import wraps
+
+from fastapi import HTTPException, status
 
 
 def file_extension_validator(file_arg_name: str, file_extensions: set[str]):
+    """
+    A decorator that validates the file extension of an uploaded file. Ensures that the provided file has an allowed
+    extension before executing the decorated function. Raises an HTTPException if the validation fails.
+
+    :param file_arg_name: The name of the argument in the decorated function's signature that is expected to
+        contain the file object.
+    :param file_extensions: A set of allowed file extensions (case-insensitive) for the uploaded file.
+    :return: The decorated function with added file extension validation logic.
+    :rtype: Callable
+    """
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
             if file_arg_name in kwargs:
+                file = kwargs.get(file_arg_name)
+                if file and (hasattr(file, "filename")):
 
-                file_arg = kwargs.get(file_arg_name)
+                    filename: str | None = file.filename
 
-                if not isinstance(file_arg, UploadFile):
-                    return await func(*args, **kwargs)
+                    if not filename:
+                        raise HTTPException(
+                            status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"filename is empty",
+                        )
 
-                file: UploadFile = file_arg
+                    _, ext = os.path.splitext(filename)
 
-                filename: str | None = file.filename
+                    file_extension = ext[1:].lower()
 
-                if not filename:
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail=f"filename is empty",
-                    )
-
-                _, ext = os.path.splitext(filename)
-
-                file_extension = ext[1:].lower()
-
-                if file_extension not in file_extensions:
-                    raise HTTPException(
-                        status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-                        detail=f"{file_extension} type is unavailable",
-                    )
+                    if file_extension not in file_extensions:
+                        raise HTTPException(
+                            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                            detail=f"{file_extension} type is unavailable",
+                        )
 
             return await func(*args, **kwargs)
 
--- file_validator/file_size_validator.py
+++ file_validator/file_size_validator.py
@@ -1,31 +1,38 @@
-from fastapi import UploadFile, HTTPException, status
 from functools import wraps
+
+from fastapi import HTTPException, status
 
 
 def file_size_validator(file_arg_name: str, max_size_mb: int):
+    """
+    Decorator to validate the size of a file passed as a keyword argument to an asynchronous function. The decorator
+    checks if the file's size exceeds the specified maximum size in megabytes and raises an HTTPException if the
+    limit is exceeded.
+
+    :param file_arg_name: The name of the keyword argument representing the file in the decorated function.
+    :param max_size_mb: The maximum file size allowed in megabytes.
+    :return: A decorator function that validates the file's size against the specified limit.
+    :rtype: Callable
+    """
     max_size_bytes = max_size_mb * 1024 * 1024
 
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
             if file_arg_name in kwargs:
-                file_arg = kwargs.get(file_arg_name)
+                file = kwargs.get(file_arg_name)
 
-                if not isinstance(file_arg, UploadFile):
-                    return await func(*args, **kwargs)
+                if file and (hasattr(file, "size")):
+                    file_size: int | None = file.size
 
-                file: UploadFile = file_arg
+                    if not file_size:
+                        return await func(*args, **kwargs)
 
-                file_size: int | None = file.size
-
-                if not file_size:
-                    return await func(*args, **kwargs)
-
-                if file_size > max_size_bytes:
-                    raise HTTPException(
-                        status_code=status.HTTP_413_CONTENT_TOO_LARGE,
-                        detail=f"file size is bigger than {max_size_mb}MB",
-                    )
+                    if file_size > max_size_bytes:
+                        raise HTTPException(
+                            status_code=status.HTTP_413_CONTENT_TOO_LARGE,
+                            detail=f"file size is bigger than {max_size_mb}MB",
+                        )
 
             return await func(*args, **kwargs)
 
